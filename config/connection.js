5e7dd320a39818b4c2869d9f897b4d93a49f9bf9
Vincent Nguyen <vnguyen2@gmail.com>
2016-11-12 10:51:53 -0600

    test

--- config/connection.js
+++ config/connection.js
@@ -10,10 +10,10 @@ var source = {
 	},
 	jawsDB: {
 		port: 3306,
-		host: 'tviw6wn55xwxejwj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-		user: 'z8685p3q4g91axid',
-		password: 'h0bfgqmcp8g5zoru',
-		database: 'vrdvw85q1f5m4sbq'
+		host: 'enqhzd10cxh7hv2e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+		user: 'x0c27m6z929hzxc1',
+		password: 'sot1flh15qsmi6qe',
+		database: 'ovxfj2kpju3vwb0k'
 	}
 }
 
